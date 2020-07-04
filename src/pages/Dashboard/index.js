336197cb7ab22fd272b5e48d28244c2c8a11ab3b
Leonardo Flores <tuxeww6@gmail.com>
2020-07-04 15:08:14 -0300

    bug de sempre do react aaaaa

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -8,12 +8,8 @@ import {
 import "./styles.css";
 import logo from "../../assets/logo-ml.png";
 import imageUser from "../../assets/profile.jpg";
-<<<<<<< HEAD
-import ButtonBlue from "../../components/ButtonBlue"
-=======
 import ButtonBlue from "../../components/ButtonBlue";
 import StatusDelivery from "../../components/StatusDelivery";
->>>>>>> 5385b75ab24fc1415e7ad1cae94bc5cde05f3298
 
 export default function Dashboard() {
   return (
@@ -38,43 +34,24 @@ export default function Dashboard() {
         <div className="div-content-card">
           <div className="div-card-info">
             <div className="div-header-card-info">
-<<<<<<< HEAD
               <h2>Verificação de Produtos</h2>
               <h3>Produtos Adicionados</h3>
             </div>
             <div className="div-card-selected-items">
-                  <div className="div-selected-item">
-                    <span>Psp 3000 Original Playstation Na Caixa S/ Bateria Otimo Estado</span>
-                    <span>1x</span>
-                  </div>
+              <div className="div-selected-item">
+                <span>
+                  Psp 3000 Original Playstation Na Caixa S/ Bateria Otimo Estado
+                </span>
+                <span>1x</span>
+              </div>
             </div>
             <div className="checkout-area">
               <div className="label-checkout">
-               <span>Total de produtos: 1</span>
-               <span>Peso total: 3gr</span>
+                <span>Total de produtos: 1</span>
+                <span>Peso total: 3gr</span>
               </div>
               <div>
                 <ButtonBlue title="Chamar entregador"></ButtonBlue>
-=======
-              <h2>Verificação de Saída</h2>
-              <h3>Produtos adicionados:</h3>
-            </div>
-            <div className="div-card-product">
-              <div className="card-product">
-                <h4>
-                  Psp 3000 Original Playstation Na Caixa S/ Bateria Otimo Estado
-                </h4>
-                <h4>1x</h4>
-              </div>
-            </div>
-            <div className="div-card-info-success">
-              <div className="info-product">
-                <h4>Total de produtos: 3</h4>
-                <h4>Peso total: 300gr </h4>
-              </div>
-              <div className="div-btn-confirm-driver">
-                <ButtonBlue title="Chamar o entregador" />
->>>>>>> 5385b75ab24fc1415e7ad1cae94bc5cde05f3298
               </div>
             </div>
           </div>
@@ -109,86 +86,86 @@ export default function Dashboard() {
                 </tr>
               </thead>
               <tbody className="list-body">
-                  <tr>
-                    <td>
-                      <img
-                        className="image-product"
-                        src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
-                        alt="ImageProduct"
-                      />
-                    </td>
-                    <td className="table-title">LG K50S Dual SIM 32 GB</td>
-                    <td>R$ 1.299</td>
-                    <td>1</td>
-                    <td>
-                      Vendido
-                      <FiCheckCircle className="check-success" />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img
-                        className="image-product"
-                        src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
-                        alt="ImageProduct"
-                      />
-                    </td>
-                    <td className="table-title">LG K50S Dual SIM 32 GB</td>
-                    <td>R$ 1.299</td>
-                    <td>1</td>
-                    <td>
-                      Vendido
-                      <FiCheckCircle className="check-success" />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img
-                        className="image-product"
-                        src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
-                        alt="ImageProduct"
-                      />
-                    </td>
-                    <td className="table-title">LG K50S Dual SIM 32 GB</td>
-                    <td>R$ 1.299</td>
-                    <td>1</td>
-                    <td>
-                      Vendido
-                      <FiCheckCircle className="check-success" />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img
-                        className="image-product"
-                        src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
-                        alt="ImageProduct"
-                      />
-                    </td>
-                    <td className="table-title">LG K50S Dual SIM 32 GB</td>
-                    <td>R$ 1.299</td>
-                    <td>1</td>
-                    <td>
-                      Vendido
-                      <FiCheckCircle className="check-success" />
-                    </td>
-                  </tr>
-                  <tr>
-                    <td>
-                      <img
-                        className="image-product"
-                        src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
-                        alt="ImageProduct"
-                      />
-                    </td>
-                    <td className="table-title">LG K50S Dual SIM 32 GB</td>
-                    <td>R$ 1.299</td>
-                    <td>1</td>
-                    <td>
-                      Vendido
-                      <FiCheckCircle className="check-success" />
-                    </td>
-                  </tr>
+                <tr>
+                  <td>
+                    <img
+                      className="image-product"
+                      src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
+                      alt="ImageProduct"
+                    />
+                  </td>
+                  <td className="table-title">LG K50S Dual SIM 32 GB</td>
+                  <td>R$ 1.299</td>
+                  <td>1</td>
+                  <td>
+                    Vendido
+                    <FiCheckCircle className="check-success" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img
+                      className="image-product"
+                      src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
+                      alt="ImageProduct"
+                    />
+                  </td>
+                  <td className="table-title">LG K50S Dual SIM 32 GB</td>
+                  <td>R$ 1.299</td>
+                  <td>1</td>
+                  <td>
+                    Vendido
+                    <FiCheckCircle className="check-success" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img
+                      className="image-product"
+                      src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
+                      alt="ImageProduct"
+                    />
+                  </td>
+                  <td className="table-title">LG K50S Dual SIM 32 GB</td>
+                  <td>R$ 1.299</td>
+                  <td>1</td>
+                  <td>
+                    Vendido
+                    <FiCheckCircle className="check-success" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img
+                      className="image-product"
+                      src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
+                      alt="ImageProduct"
+                    />
+                  </td>
+                  <td className="table-title">LG K50S Dual SIM 32 GB</td>
+                  <td>R$ 1.299</td>
+                  <td>1</td>
+                  <td>
+                    Vendido
+                    <FiCheckCircle className="check-success" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <img
+                      className="image-product"
+                      src="https://http2.mlstatic.com/D_NQ_NP_2X_854957-MLA41827614180_052020-F.webp%202x"
+                      alt="ImageProduct"
+                    />
+                  </td>
+                  <td className="table-title">LG K50S Dual SIM 32 GB</td>
+                  <td>R$ 1.299</td>
+                  <td>1</td>
+                  <td>
+                    Vendido
+                    <FiCheckCircle className="check-success" />
+                  </td>
+                </tr>
               </tbody>
             </table>
             <StatusDelivery />
--- src/pages/DashboardDriver/index.js
+++ src/pages/DashboardDriver/index.js
@@ -6,21 +6,20 @@ import React from "react";
 //   FiCheckCircle,
 // } from "react-icons/fi";
 import "./styles.css";
-import NavBar from '../../components/NavBar'
+import NavBar from "../../components/NavBar";
 // import logo from "../../assets/logo-ml.png";
 // import imageUser from "../../assets/profile.jpg";
 
 export default function DashboardDriver() {
   return (
-      <>
-        <div className="div-root-panel">
-            <NavBar/>
-            <div className="buttonCard">
-                <button className="button">Pesquisar</button>
-            </div>
-            {/* <img alt="map" className="map-img" src="https://www.umhs-adolescenthealth.org/wp-content/uploads/2016/12/google-map-background.jpg"></img> */}
+    <>
+      <div className="div-root-panel">
+        <NavBar />
+        <div className="buttonCard">
+          <button className="button">Pesquisar</button>
         </div>
-      </>
-  )
-
+        {/* <img alt="map" className="map-img" src="https://www.umhs-adolescenthealth.org/wp-content/uploads/2016/12/google-map-background.jpg"></img> */}
+      </div>
+    </>
+  );
 }
