61cf97b5bfb1395f9721e53841c7c2837583ffac
Leonardo Flores <tuxeww6@gmail.com>
2020-07-04 21:42:27 -0300

    update

--- src/components/LoginCard/index.js
+++ src/components/LoginCard/index.js
@@ -1,43 +1,67 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import api from "../../services/api";
 import ButtonBlue from "../../components/ButtonBlue";
+import { validate, required, email } from "sm-form-validation";
 import { FiArrowRight } from "react-icons/fi";
-import { useHistory  } from 'react-router-dom'
-import api from '../../services/api'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useHistory } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function LoginCard(props) {
-  const notify = (message) => toast.error(message)
+  const notify = (message) => toast.error(message);
+  const history = useHistory();
+  const [spinner, setSpinner] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-  const history = useHistory()
-  const [formData,setFormData] = useState({
-      email:"",
-      password:""
-  })
+  const handleLogin = () => RequestLogin(formData);
 
-  const handleLogin = () => (
-    formData.email && formData.password ? RequestLogin(formData) : notify('Preencha todos os campos')
-  )
+  function RequestLogin(data) {
+    setSpinner(true);
+    api
+      .post("/login", data)
+      .then((res) => {
+        localStorage.setItem("token", res.data.data.token);
+        localStorage.setItem("userData", JSON.stringify(res.data.data.user));
 
-  function RequestLogin(data){
-    api.post('/login',data)
-      .then((res)=>{
-          localStorage.setItem('token',res.data.data.token)
-          localStorage.setItem('userData',JSON.stringify(res.data.data.user))
+        const userData = JSON.parse(localStorage.getItem("userData"));
 
-          const userData =  JSON.parse(localStorage.getItem('userData'))
-          
-          if(userData.type === 'S'){
-            return  history.push('/dashboard')
-          }else{
-            return  history.push('/dashboardDriver')
-          }
-        })
-      .catch(err=>{
-        notify(err.response.data.message)
-      })    
+        if (userData.type === "S") {
+          return history.push("/dashboard");
+        } else {
+          return history.push("/dashboardDriver");
+        }
+        setSpinner(false);
+      })
+      .catch((err) => {
+        notify(err.response.data.message);
+        setSpinner(false);
+      });
   }
+
+  function handleValidateForm() {
+    validate([
+      {
+        [formData.email]: [
+          email("Email inválido, tente outro novamente!"),
+          required("Campo email é obrigatório"),
+        ],
+      },
+      {
+        [formData.password]: [required("Campo senha é obrigatório")],
+      },
+    ])
+      .then(() => {
+        handleLogin();
+      })
+      .catch((err) => {
+        notify(err[0].status);
+      });
+  }
+
   return (
     <div className="div-login-card">
       <ToastContainer />
@@ -45,18 +69,37 @@ export default function LoginCard(props) {
         <h2>Faça seu logon</h2>
       </div>
       <div className="div-input-login">
-        <input className="input" type="text" placeholder="Email" onChange={(e)=> setFormData({...formData,email:e.target.value}) } />
-        <input className="input" type="password" placeholder="Senha" onChange={(e)=> setFormData({...formData,password:e.target.value}) } />
+        <input
+          className="input"
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          required
+        />
+        <input
+          className="input"
+          type="password"
+          placeholder="Senha"
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+          required
+        />
         <a className="link-forgot-password" href="https://google.com">
           esqueci minha senha
         </a>
       </div>
       <div>
-        <ButtonBlue title="entrar" onClick={handleLogin} />
+        <ButtonBlue
+          title="entrar"
+          loading={spinner}
+          onClick={handleValidateForm}
+        />
       </div>
       <div className="div-go-create-account">
         <span className="link-go-create-account" onClick={props.clickedLink}>
           Criar nova conta
+          {spinner}
           <FiArrowRight />
         </span>
       </div>
--- src/components/RegisterCard/index.js
+++ src/components/RegisterCard/index.js
@@ -1,11 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import "./styles.css";
+import api from "../../services/api";
+import cep from "cep-promise";
 import ButtonBlue from "../../components/ButtonBlue";
 import { FiArrowLeft } from "react-icons/fi";
+// import { validate, required, email } from "sm-form-validation";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function RegisterCard(props) {
+  const notify = (message) => toast.error(message);
+  const history = useHistory();
+  const [spinner, setSpinner] = useState(false);
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    document: "",
+    type: "",
+    phone: {
+      areaCode: "",
+      number: "",
+    },
+    address: {
+      street: "",
+      district: "",
+      zipCode: "",
+      city: "",
+      state: "",
+      number: "",
+      complement: "",
+    },
+    car: {
+      model: "Uno mile 2000 1.0",
+      plate: "IVTX5420",
+      measures: {
+        height: "30",
+        width: "40",
+        length: "50",
+      },
+    },
+  });
+
+  const handleRegister = () => RequestRegister(formData);
+
+  function RequestRegister(data) {
+    setSpinner(true);
+    api
+      .post("/api/register", data)
+      .then((res) => {
+        localStorage.setItem("token", res.data.data.token);
+        localStorage.setItem("userData", JSON.stringify(res.data.data.user));
+        const userData = JSON.parse(localStorage.getItem("userData"));
+
+        if (userData.type === "S") {
+          return history.push("/dashboard");
+        } else {
+          return history.push("/dashboardDriver");
+        }
+
+        setSpinner(false);
+      })
+      .catch((err) => {
+        notify(err.response.data.message);
+        setSpinner(false);
+      });
+  }
+
+  function handleGetCep() {
+    if (formData.address.zipCode.length === 8) {
+      console.log(formData);
+      cep(formData.address.zipCode)
+        .then((response) => {
+          setFormData({
+            ...formData,
+            address: {
+              street: response.street,
+              neighborhood: response.neighborhood,
+              city: response.city,
+              state: response.state,
+            }
+          });
+
+        })
+        .catch((err) => {
+          notify("Ocorreu um erro na requisição");
+        });
+    }
+  }
+
   return (
     <div className="div-register-card">
+      <ToastContainer />
       <div className="div-title-card-register">
         <h2>Crie sua conta já!</h2>
       </div>
@@ -14,41 +103,140 @@ export default function RegisterCard(props) {
           className="input row-input"
           type="text"
           placeholder="Nome completo"
+          onChange={(e) => {
+            setFormData({ ...formData, name: e.target.value });
+          }}
         />
-        <input className="input row-input" type="text" placeholder="Email" />
+        <input
+          className="input row-input"
+          type="text"
+          placeholder="Email"
+          onChange={(e) => {
+            setFormData({ ...formData, email: e.target.value });
+          }}
+        />
+        <input
+          className="input row-input"
+          type="text"
+          placeholder="CPF"
+          onChange={(e) => {
+            setFormData({ ...formData, document: e.target.value });
+          }}
+        />
       </div>
       <div className="div-input">
-        <input className="input row-input" type="text" placeholder="CPF" />
         <input
           className="input row-input-area-code"
           type="text"
           placeholder="DDD"
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              [formData.phone.areaCode]: e.target.value,
+            });
+          }}
         />
         <input
           className="input row-input-phone"
           type="text"
           placeholder="Celular"
         />
-      </div>
-      <div className="div-input">
-        <input className="input row-input" type="text" placeholder="CEP" />
+        <input
+          className="input row-input"
+          type="text"
+          placeholder="CEP"
+          onBlur={handleGetCep}
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              address: {
+                ...formData.address,
+                zipCode: e.target.value,
+              },
+            });
+          }}
+        />
         <input
           className="input row-input"
           type="text"
           placeholder="Logradouro"
+          // value={formData.address.street}
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              [formData.address.street]: e.target.value,
+            });
+          }}
         />
       </div>
       <div className="div-input">
         <input className="input row-input" type="text" placeholder="Número" />
-        <input className="input row-input" type="text" placeholder="Bairro" />
+        <input
+          className="input row-input"
+          type="text"
+          placeholder="Bairro"
+          // value={formData.address.district}
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              [formData.address.district]: e.target.value,
+            });
+          }}
+        />
+        <input
+          className="input row-input"
+          type="text"
+          placeholder="Cidade"
+          // value={formData.address.city}
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              [formData.address.city]: e.target.value,
+            });
+          }}
+        />
       </div>
       <div className="div-input">
-        <input className="input row-input" type="text" placeholder="Cidade" />
+        <input
+          className="input row-input"
+          type="text"
+          placeholder="Estado"
+          // value={[formData.address.state]}
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              [formData.address.state]: e.target.value,
+            });
+          }}
+        />
         <input
           className="input row-input"
           type="text"
           placeholder="Complemento"
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              [formData.address.complement]: e.target.value,
+            });
+          }}
         />
+        <select
+          className="input row-input"
+          type="text"
+          onChange={(e) => {
+            setFormData({
+              ...formData,
+              [formData.type]: e.target.value,
+            });
+          }}
+          required
+        >
+          <option value="" selected>
+            Selecione um tipo
+          </option>
+          <option value="S">Vendedor</option>
+          <option value="D">Motorista</option>
+        </select>
       </div>
       <div className="div-btn-terms">
         <h4>
@@ -57,7 +245,11 @@ export default function RegisterCard(props) {
           <b> Políticas de privacidade </b>
           do Mercado Livre e do Mercado Pago.
         </h4>
-        <ButtonBlue title="Criar conta" />
+        <ButtonBlue
+          title="Criar conta"
+          loading={spinner}
+          onClick={handleRegister}
+        />
       </div>
       <div className="div-go-create-account">
         <span className="link-go-create-account" onClick={props.clickedLink}>
